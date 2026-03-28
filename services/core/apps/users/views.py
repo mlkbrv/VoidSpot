1fe3d502fb20d554a15f8fa2a6a1194ec9a7f2c1
E <emilakbarov857@gmail.com>
2026-03-28 22:51:40 +0400

    feat(users): added views for logout change password and register

--- services/core/apps/users/views.py
+++ services/core/apps/users/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework import generics,status,permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import *
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserProfileSerializer
+
+class LogoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except KeyError:
+            return Response({
+                'error': 'Refresh Token is missing'
+            },status=status.HTTP_400_BAD_REQUEST)
+        except TokenError:
+            return Response({
+                'error': 'Invalid Token'
+            },status=status.HTTP_400_BAD_REQUEST)
+
+class ChangePasswordView(generics.UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        user = self.get_object()
+
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        if not user.check_password(serializer.data['old_password']):
+            return Response({
+                'error': 'Old Password Incorrect'
+            },status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(serializer.validated_data.get('new_password'))
+        user.save()
+
+        return Response({'status': 'OK'}, status=status.HTTP_200_OK)
