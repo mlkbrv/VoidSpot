ddf6a02275c4449fe131c52446e0e0b85e064d60
E <emilakbarov857@gmail.com>
2026-04-14 11:21:16 +0400

    add posts models

--- services/core/apps/posts/models.py
+++ services/core/apps/posts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+import uuid
 
-# Create your models here.
+user = get_user_model()
+
+class Media(models.Model):
+    media = models.FileField(upload_to='posts/%Y/%m/%d/',null=True,blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    media = models.ManyToManyField(Media,blank=True,related_name='posts')
+    author = models.ForeignKey(user,on_delete=models.CASCADE,related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Tweet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.TextField(blank=False,null=False)
+    author = models.ForeignKey(user,on_delete=models.CASCADE,related_name='tweets')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
